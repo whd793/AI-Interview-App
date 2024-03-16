4ae4bb0d595d8b75f1beabb78c60b20249e1e7d0
Won Lee <whd793@gmail.com>
2024-03-16 14:26:47 +0900

    added drizzle orm postgres schema

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -6,7 +6,7 @@ function Dashboard() {
     <div className='p-10'>
       <h2 className='font-bold text-3xl text-primary'>Dashboard</h2>
       <h2 className='text-gray-500'>
-        Create and Start your AI Mockup Interview
+        Create and Start your AI Mockup Interview - 새로운 면접을 만들어보세요
       </h2>
 
       <div className='grid grid-cols-1 md:grid-cols-3 my-5 gap-5'></div>
--- drizzle.config.js
+++ drizzle.config.js
@@ -1,8 +1,8 @@
 /** @type { import("drizzle-kit").Config } */
 export default {
-    schema: "./utils/schema.js",
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: 'postgresql://accounts:ypvN2ke3PGFE@ep-weathered-heart-a58wmzem.us-east-2.aws.neon.tech/ai-interview-mocker?sslmode=require',
-    }
-  };
+  schema: './utils/schema.js',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: 'postgresql://AI-Interview_owner:skmQ4Sv8iHRf@ep-twilight-snowflake-a5o13eqs.us-east-2.aws.neon.tech/AI-Interview?sslmode=require',
+  },
+};
