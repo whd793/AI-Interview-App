698ed0ef3f2169003d5399e641c00f5892701825
Won Lee <whd793@gmail.com>
2024-03-16 13:46:47 +0900

    added dashboard - redirection from signin to dashboard

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,19 @@
+import { UserButton } from '@clerk/nextjs';
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div className='p-10'>
+      <h2 className='font-bold text-3xl text-primary'>Dashboard</h2>
+      <h2 className='text-gray-500'>
+        Create and Start your AI Mockup Interview
+      </h2>
+
+      <div className='grid grid-cols-1 md:grid-cols-3 my-5 gap-5'></div>
+
+      {/* Previous Interview List  */}
+    </div>
+  );
+}
+
+export default Dashboard;
