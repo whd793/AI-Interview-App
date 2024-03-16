a02d5f423a8dfbb242e2159335f4e7a4c1ff568b
Won Lee <whd793@gmail.com>
2024-03-16 13:26:47 +0900

    style change landing page

--- app/page.js
+++ app/page.js
@@ -6,12 +6,17 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <section className='bg-white dark:bg-gray-900'>
-        <div className='py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12'>
-          <h1 className='mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
-            AI 면접 연습
+      <section className=''>
+        <img
+          alt=''
+          src='/images/inv.jpg'
+          className=' absolute inset-0 h-full w-full object-cover brightness-[40%] z-[-1]'
+        />
+        <div className='py-32 px-4 mx-auto max-w-screen-xl text-center lg:py-32 lg:px-12 z-10'>
+          <h1 className='mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl dark:text-white'>
+            AI 취업 면접 연습 도우미
           </h1>
-          <p className='mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400'>
+          <p className='mb-8 text-lg font-normal text-gray-100 lg:text-xl sm:px-16 xl:px-48 '>
             AI 면접으로 연습해서 취업률을 높여보세요!
           </p>
           <div className='flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4'>
